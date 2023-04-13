f5237af16f184350e4aa4376ba7783d0fd5e5e44
eniolastyle <amiolastyle@gmail.com>
2023-04-13 18:15:58 +0100

    memories app server

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/posts';
+const url = 'ec2-3-91-208-104.compute-1.amazonaws.com';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
