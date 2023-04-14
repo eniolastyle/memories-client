ba5c70ead82eb1a3b7d82095736439ca2f67d332
eniolastyle <amiolastyle@gmail.com>
2023-04-14 15:02:35 +0100

    memories app client

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'ec2-3-91-208-104.compute-1.amazonaws.com';
+const url = 'ec2-3-86-177-222.compute-1.amazonaws.com';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
